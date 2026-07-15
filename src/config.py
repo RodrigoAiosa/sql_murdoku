ac6327f11e4bd0b455ee2d80dee6e849a42d50c0
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-14 21:48:12 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -45,7 +45,7 @@ GOOGLE_SHEET_PUBHTML_URL = (
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vQwQTp7f0ph0nfCwOelM9XgUSi-e8-EMYDHe6"
     "Ef8WOy8GyKvzU8ZrXq00out2818c8WJ51FEHWKQTYG/pubhtml"
 )
-GOOGLE_SCRIPT_WEBAPP_URL = ""  # <-- cole aqui a URL /exec do seu Apps Script (ver README)
+GOOGLE_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbw1Xhktq6GnYV5exfQPCNpcw0XKVDItk9VeF7JWEHZ62KpIl8PJhs8IC7j1ZdI2T2VGMQ/exec"  # <-- cole aqui a URL /exec do seu Apps Script (ver README)
 
 # ----------------------------------------------------------------------
 # Aparência / geral
